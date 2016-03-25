ad7a54a75cfed3cec83f46335190f188c4877a68
Tiani Ball <tianifball@gmail.com>
2016-03-25 00:39:22 -0500

    Added files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$("h1, h2, h3").css("color", "darkgreen");
+$("li").css("color", "darkred");
+$("h1, h2, h3, p, footer").css("text", "center");
+$("h1").css("font-family", "Arial Black", "bold");
+$("h2").css("font-family", "gadget");
+$("p").css("font-size", "100%", "font-family", "Georgia");
+$("body").css("background-color", "lightgrey");
